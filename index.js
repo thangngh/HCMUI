070b0e6415c252403a1c6cbf24cb3df5483ef1c6
pv <79627101+thangngh@users.noreply.github.com>
2022-03-19 09:58:54 +0700

    lastcm

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const dropDownMenu = document.querySelectorAll('.dropdown-menu');
 const dropDown = document.querySelectorAll('.dropdown');
-const dropSearch = document.querySelector('.drop-search');
-const navSearch = document.querySelector('.nav-search');
+const dropSearch = document.querySelectorAll('.drop-search');
+const navSearch = document.querySelectorAll('.nav-search');
+const mbNav = document.querySelector('.mb-nav-navbar');
+const mbIcon = document.querySelector('.mb-icon');
 
 dropDown.forEach((element, v) => {
     element.addEventListener('mouseleave', () => {
@@ -22,6 +24,26 @@ dropDown.forEach((element, v) => {
     });
 });
 
-navSearch.addEventListener('click', () => {
-    dropSearch.classList.toggle('hidden');
+navSearch.forEach((element, v) => {
+    element.addEventListener('click', () => {
+        dropSearch.forEach((e, ix) => {
+            e.classList.toggle('hidden');
+        });
+    });
+});
+
+mbIcon.addEventListener('click', () => {
+    mbNav.classList.toggle('hidden');
+});
+
+$(document).ready(function () {
+    $('.carousel-inner').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow:
+            "<button type='button' class='slick-prev pull-left'><span class='material-icons' aria-hidden='true'>chevron_left</span></button>",
+        nextArrow:
+            "<button type='button' class='slick-next pull-right'><span class='material-icons' aria-hidden='true'>navigate_next</span></button>",
+    });
 });
